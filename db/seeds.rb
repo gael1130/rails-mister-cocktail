febd13047dd248ac9c4b54ebf0c65407e4e022e6
gael gendre <gael.gendre@gmail.com>
2020-11-12 17:08:23 +0100

    still need to work on controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+ingredient["drinks"].each do |element|
+  puts "showing #{element['strIngredient1']}"
+  new_ingredient = Ingredient.create( name: element['strIngredient1'])
+end
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+
+# data = JSON.parse(serialized_data)
+# data['drinks'].each do |ingredient|
+#   puts "Printing #{ingredient['strIngredient1']}"
+#   new_ingredient = Ingredient.new( name: ingredient['strIngredient1'] )
+#   new_ingredient.save!
+# end
+
+
